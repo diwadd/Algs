717a840993ae86701a152c2b8bde8801cf9a0790
Dawid Dul <dawid.dul@gmail.com>
2017-03-21 18:23:49 +0100

    Heap array and Heap sort implemented.

--- main_heap.py
+++ main_heap.py
@@ -1,10 +1,12 @@
 import binary_heap as bh
 
-a = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+
+
+a = bh.Array([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
 
 print("Testing the heap")
 print(a)
-bh.max_heapify(a, 2)
+bh.max_heapify(a, 1)
 
 #print(a)
 #bh.max_heapify(a, 3)
@@ -14,8 +16,20 @@ print(a)
 
 print("Building a heap")
 
-b = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+b = bh.Array([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
 print(b)
 
 bh.build_max_heap(b)
 print(b)
+
+
+c = bh.Array([5,4,2,7,4,37,8,7])
+print(c)
+bh.heapsort(c)
+print(c)
+
+
+
+h = bh.Array([1,2,3,4,5,6])
+print(h)
+print(h.heap_size)
---
+++ src/binary_heap.py
@@ -0,0 +1,113 @@
+import math
+
+
+class Array(list):
+    def __init__(self, *args):
+        list.__init__(self, *args)
+        self.heap_size = len(self)
+
+
+def exchange(a, b):
+    t = a
+    a = b
+    b = t
+
+
+def parent(i):
+    """
+    Return parent of node i.
+    For arrays indexed from 1 to N floor(i/2).
+    :param i: node index
+    :return: parent index
+    """
+    return math.floor((i - 1) / 2)
+
+    
+def left(i):
+    """
+    Return the left child of nod with index i.
+    For arrays indexed from 1 to N 2 * i.
+    """
+    return 2 * i + 1
+ 
+ 
+def right(i):
+    """
+    Return the right child of nod with index i.
+    For arrays indexed from 1 to N 2 * i + 1.
+    """
+    return 2 * i + 2
+    
+
+def max_heapify(a, i):
+    """"
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 154, Section 6.2
+    
+    :param a: An array
+    :param i: Index in a.
+    """
+    le = left(i)
+    ri = right(i)
+    largest = None
+
+
+    # Python lists are dynamic so a.heap-size = len(a)
+    # For arrays indexed from 1 le <= len(a)
+    if le < a.heap_size and a[le] > a[i]:
+        largest = le
+    else:
+        largest = i
+    # For arrays indexed from 1 ri <= len(a)
+    if ri < a.heap_size and a[ri] > a[largest]:
+        largest = ri
+    if largest != i:
+        # exchange a[i] with a[largest]
+        a[i], a[largest] = a[largest], a[i]
+        max_heapify(a, largest)
+
+
+def build_max_heap(a):
+    """
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 157, Section 6.3
+
+    Transform an array into a heap.
+
+    :param a: An array.
+
+    """
+    for i in range(math.floor((len(a) - 1)/2), -1, -1):
+        max_heapify(a, i)
+
+
+def heapsort(a):
+    """
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 160, Section 6.4
+
+    Sort an array with heap sort.
+
+    :param a: An array.
+
+    """
+
+    build_max_heap(a)
+
+    for i in range(len(a) - 1, 0, -1):
+        # exchange a[0] with a[i]
+        a[0], a[i] = a[i], a[0]
+        a.heap_size = a.heap_size - 1
+        max_heapify(a, 0)
+        
+    
+
+    
+    
+    
+    
+    
+    
