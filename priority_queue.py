8308eb0d3fdc55f9755ad34dc7e3a239f2b4ea51
Dawid Dul <dawid.dul@gmail.com>
2017-03-22 18:31:25 +0100

    Binary heap implemented.

--- priority_queue.py
+++ priority_queue.py
@@ -7,6 +7,17 @@ def heap_maximum(a):
 
 
 def heap_extract_max(a):
+    """
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 163, Section 6.5
+
+    Removes and returns the largest element.
+
+    :param a: An array.
+
+    """
+
     if a.heap_size < 1:
         return ValueError
 
@@ -19,3 +30,57 @@ def heap_extract_max(a):
     bh.max_heapify(a, 0)
 
     return max_value
+
+
+def heap_increase_key(a, i, key):
+    """
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 164, Section 6.5
+
+    Increases the value of element a[i] to key.
+    Condition: key >= a[i].
+
+    :param a: An array.
+    :param i: Index in a.
+    :param key: New value for a[i].
+    """
+
+
+    if key < a[i]:
+        return ValueError
+
+    a[i] = key
+    while i > 0 and a[bh.parent(i)] < a[i]:
+        a[i], a[bh.parent(i)] = a[bh.parent(i)], a[i]
+        i = bh.parent(i)
+
+
+def max_heap_insert(a, key):
+    """
+    Intorduction to algorithms 3rd ed.
+    Cormen et al.
+    Page 164, Section 6.5
+
+    Insert a new element into the heap.
+
+    :param a: An array.
+    :param key: New element to be inserted into the heap.
+    """
+
+    a.heap_size = a.heap_size + 1
+    a.append(-math.inf)
+    heap_increase_key(a, a.heap_size - 1, key)
+
+    
+
+
+
+
+
+
+
+
+
+
+
--- test_priority_queue.py
+++ test_priority_queue.py
@@ -6,12 +6,32 @@ import priority_queue as pq
 class TestPriorityQueue(unittest.TestCase):
 
     def test_heap_extract_max(self):
-        a = bh.Array([2, 3, 10, 4, 16, 10, 11])
+        a = bh.Array([3, 16, 7, 6])
         bh.build_max_heap(a)
 
         max_value = pq.heap_extract_max(a)
 
         self.assertEqual(max_value, 16)
+        self.assertEqual(a, [7, 6, 3])
+        
+    def test_heap_increase_key(self):
+        a = bh.Array([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+        bh.build_max_heap(a)
+
+        pq.heap_increase_key(a, 8, 15)
+        b = bh.Array([16, 15, 10, 14, 7, 9, 3, 2, 8, 1])
+
+        self.assertEqual(a, b)
+
+    def test_max_heap_insert(self):
+        a = bh.Array([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+        bh.build_max_heap(a)
+
+        pq.max_heap_insert(a, 20)
+        b = bh.Array([20, 16, 10, 8, 14, 9, 3, 2, 4, 1, 7])
+
+        self.assertEqual(a, b)
+
 
 
 if __name__ == '__main__':
